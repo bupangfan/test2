54aee59779e5806d72685ff95833641eb3f5b873
wzf <wzfqaz789@qq.com>
2022-03-03 21:35:10 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,17 @@
+import requests
+import pytest
+
+
+class TestClass(object):
+
+    def test_one(self):
+        print("用例1执行完成")
+        pass
+
+    def test_two(self):
+        print("用例2执行完成")
+        pass
+
+
+
+
